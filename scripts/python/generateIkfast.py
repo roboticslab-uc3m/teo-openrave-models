001be5f1d13d25df8845a9fd598108fa9114ce03
jgvictores <jcgvicto@gmail.com>
2018-03-23 18:39:56 +0100

    tidy up

--- scripts/python/generateIkfast.py
+++ scripts/python/generateIkfast.py
@@ -1,27 +1,33 @@
-##
-# <b>Legal</b> 
-#
-# Copyright: (C) 2017 Universidad Carlos III de Madrid
-#
+#!/usr/bin/env python
+
+# Copyright: (C) 2018 Universidad Carlos III de Madrid
 # Author: Raul de Santos Rico
-#
 # CopyPolicy: Released under the terms of the LGPLv2.1 or later, see license/LGPL.TXT
 
 from openravepy import *
-from numpy import *
-import numpy, time
-from openravepy.misc import InitOpenRAVELogging
-InitOpenRAVELogging() # initialized with a logger. Allow us to log IKFast information 
+from openravepy.misc import *
 
-env = Environment()
-env.SetViewer('qtcoin')
-env.Load('/usr/local/share/teo-grasp/contexts/models/lab.env.xml')
-robot = env.GetRobots()[0]
-robot.SetActiveManipulator('rightArm_trunk')
+try:
+    RaveInitialize()
+    InitOpenRAVELogging()  # From misc, handles: No handlers could be found for logger "openravepy.databases.convexdecomposition"
 
-ikmodel = databases.inversekinematics.InverseKinematicsModel(robot, iktype=IkParameterizationType.Transform6D)
-if not ikmodel.load():
-    print 'Generating IK equation of: %s (one time computation)...' %robot.GetActiveManipulator().GetName()
-    ikmodel.autogenerate()
-else:
-    print 'Inverse Kinematic of %s loaded!' %robot.GetActiveManipulator().GetName()
+    env = Environment()
+    env.SetViewer('qtcoin')
+    env.Load('/usr/local/share/teo-openrave-models/contexts/openrave/teo/teo.robot.xml')
+
+    # ikfasr
+    robot = env.GetRobots()[0]
+    robot.SetActiveManipulator('rightArm_trunk')
+
+    ikmodel = databases.inversekinematics.InverseKinematicsModel(robot, iktype=IkParameterizationType.Transform6D)
+    if not ikmodel.load():
+        print 'Generating IK equation of: %s (one time computation)...' %robot.GetActiveManipulator().GetName()
+        ikmodel.autogenerate()
+    else:
+        print 'Inverse Kinematic of %s loaded!' %robot.GetActiveManipulator().GetName()
+
+    while 1:
+        pass
+
+finally:
+    RaveDestroy()
