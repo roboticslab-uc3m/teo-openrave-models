6cd40916d8637043d1c2ff587f6322a8cab3ffdf
jgvictores <jcgvicto@gmail.com>
2018-04-04 18:57:35 +0200

    improve generateIkfast.py

--- scripts/python/generateIkfast.py
+++ scripts/python/generateIkfast.py
@@ -12,22 +12,22 @@ try:
     InitOpenRAVELogging()  # From misc, handles: No handlers could be found for logger "openravepy.databases.convexdecomposition"
 
     env = Environment()
-    env.SetViewer('qtcoin')
+    #env.SetViewer('qtcoin')  # Viewer not really required...
     env.Load('/usr/local/share/teo-openrave-models/contexts/openrave/teo/teo.robot.xml')
 
-    # ikfasr
     robot = env.GetRobots()[0]
     robot.SetActiveManipulator('rightArm_trunk')
 
     ikmodel = databases.inversekinematics.InverseKinematicsModel(robot, iktype=IkParameterizationType.Transform6D)
     if not ikmodel.load():
-        print 'Generating IK equation of: %s (one time computation)...' %robot.GetActiveManipulator().GetName()
+        print 'Generating ikfast of: %s (one time computation)...' %robot.GetActiveManipulator().GetName()
         ikmodel.autogenerate()
     else:
-        print 'Inverse Kinematic of %s loaded!' %robot.GetActiveManipulator().GetName()
+        print 'Could load existing ikfast of %s, not generating!' %robot.GetActiveManipulator().GetName()
 
-    while 1:
-        pass
+    # Can exit...
+    #while 1:
+    #    pass
 
 finally:
     RaveDestroy()
