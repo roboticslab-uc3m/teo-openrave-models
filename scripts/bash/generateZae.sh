358ddcc067dec62d0034b5a3b5e27926168651bd
jgvictores <jcgvicto@gmail.com>
2019-12-02 19:33:32 +0100

    add DAE and ZAE collada generators
    - https://github.com/roboticslab-uc3m/teo-openrave-models/issues/32

---
+++ scripts/bash/generateZae.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Authors: see AUTHORS.md at project root.
+# CopyPolicy: released under the terms of the LGPLv2.1, see LICENSE at project root.
+# URL: https://github.com/roboticslab-uc3m/teo-openrave models
+
+fileNameNoExtension="teo_lacqueyFetch"
+
+if [ $# -eq 1 ]; then
+    fileNameNoExtension="teo_$1"
+fi
+
+openrave -save $fileNameNoExtension.robot.zae /usr/local/share/teo-openrave-models/contexts/openrave/$fileNameNoExtension.robot.xml
