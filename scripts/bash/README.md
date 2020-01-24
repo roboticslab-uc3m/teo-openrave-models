0965f1d4e0a4def148c5c15d94fcf3645922dd4d
jgvictores <jcgvicto@gmail.com>
2020-01-24 12:13:59 +0100

    move processing info to README, update

---
+++ scripts/bash/README.md
@@ -0,0 +1,6 @@
+# scripts/bash/
+
+## generateConvexdecomposition.sh
+`time ./generateConvexdecomposition.sh` (Ubuntu 16.04, OpenRAVE v0.9.0, nproc=16):
+- Time `real 0m14.048s; user 0m28.001s; sys 0m0.471s`
+- Output: `convexdecomposition_0.005.pp` 5760312 (5,5M)
--- scripts/bash/generateConvexdecomposition.sh
+++ scripts/bash/generateConvexdecomposition.sh
@@ -4,6 +4,4 @@
 # CopyPolicy: released under the terms of the LGPLv2.1, see LICENSE at project root.
 # URL: https://github.com/roboticslab-uc3m/teo-openrave-models
 
-# `time ./generateConvexdecomposition.sh` with `nproc=16`: `real 0m15.307s; user 0m29.975s; sys 0m0.543s`
-# Outputs: `convexdecomposition_0.005.pp` 5802552 (5,6M).
 openrave.py --database convexdecomposition --robot=../../openrave/teo_lacqueyFetch.robot.xml --numthreads=$(nproc)
