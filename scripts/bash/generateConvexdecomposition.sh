337d1f39bc35640af7039ee58899507c424cb622
jgvictores <jcgvicto@gmail.com>
2020-01-24 11:40:48 +0100

    add comment on time
    - https://github.com/roboticslab-uc3m/teo-openrave-models/issues/3

--- scripts/bash/generateConvexdecomposition.sh
+++ scripts/bash/generateConvexdecomposition.sh
@@ -4,4 +4,5 @@
 # CopyPolicy: released under the terms of the LGPLv2.1, see LICENSE at project root.
 # URL: https://github.com/roboticslab-uc3m/teo-openrave-models
 
+# `time ./generateConvexdecomposition.sh` with `nproc=16`: `real 0m15.307s; user 0m29.975s; sys 0m0.543s`
 openrave.py --database convexdecomposition --robot=../../openrave/teo_lacqueyFetch.robot.xml --numthreads=$(nproc)
